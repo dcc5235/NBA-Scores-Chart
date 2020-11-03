e404d6853f54b19970b5534bc376bafdcacaeec0
Dany <dany@Danys-MacBook-Pro.local>
2020-11-02 21:07:46 -0600

    functionality for pts to appear in bold for winning team

--- app.js
+++ app.js
@@ -88,9 +88,17 @@ const ulParent = document.createElement('ul');
 for(let game of warriorsGames){
   const{homeTeam, awayTeam} = game;
   const gameLi = document.createElement('li');
-  const teamNames = `${awayTeam.team} @ ${homeTeam.team}`;
-  const scoreLine = `${awayTeam.points}-${homeTeam.points}`;
-  gameLi.innerText = `${teamNames} ${scoreLine}`;
+  const {team:hTeam, points: hPoints} = homeTeam;
+  const {team:aTeam, points: aPoints} = awayTeam;
+  const teamNames = `${aTeam} @ ${hTeam}`;
+  let scoreLine;
+  if (aPoints > hPoints) {
+    scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+  } else {
+    scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+  }
+  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+  console.log(scoreLine)
   ulParent.appendChild(gameLi);
 }
 
